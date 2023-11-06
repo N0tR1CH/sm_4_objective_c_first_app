b1c5e7536b025c8447b4cf725c4767e2f9dffe06
N0tR1CH <damianrichter58@gmail.com>
2023-11-06 21:47:27 +0100

    Initial commit

---
+++ zadanie4/zadanie4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zadanie4
+//
+//  Created by Damian Richter on 04/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
